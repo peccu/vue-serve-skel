230c3cacd234e56aeedb33bcc57a88bb671e82d8
peccu <peccul@gmail.com>
2018-07-12 15:43:48 +0900

    initial commit

---
+++ App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Hello vue Instant Prototyping!</h1>
+    <a target="_blank" href="https://cli.vuejs.org/guide/prototyping.html"><code>vue serve</code> document</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: [
+  ],
+  components: {
+  },
+  computed: {
+  },
+  data () {
+    return {
+    }
+  },
+  watch: {
+  },
+  beforeCreate () {
+  },
+  created () {
+  },
+  beforeMount () {
+  },
+  mounted () {
+  },
+  beforeUpdate () {
+  },
+  updated () {
+  },
+  beforeDestroy () {
+  },
+  destroyed () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+code {
+  background-color: #ccc;
+  border-radius: 2px;
+  padding: 4px;
+}
+</style>
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # vue-serve-skel
+
+skelton for Vue serve.
+
+## Usage
+
+- `git clone` to clone this repo.
+- `npm i` to install vue-cli and less etc.
+- `npx vue serve -o` to serve `App.vue` and open browser.
